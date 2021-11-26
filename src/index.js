48eabb42cf4360abb6fd16c7afa7e26e69163e51
m.nizynski <m.nizynski@ffm-online.com>
2021-11-26 20:40:30 +0100

    minor layout changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import scroll from "./scripts/scroll";
-import carousel from "./scripts/carousel";
-import menu from "./scripts/menu";
-import offerPopup from "./scripts/offerPopup";
+import {scrollTo} from "./scripts/scroll.js";
+
+const navElements = document.querySelectorAll(".start-section__nav-list-item");
+const mainButton = document.querySelector(".start-section__btn");
+
+navElements.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    scrollTo(e.target.dataset.scrollTo);
+  });
+});
+mainButton.addEventListener("click", (e) => {
+  scrollTo(e.target.dataset.scrollTo);
+});
