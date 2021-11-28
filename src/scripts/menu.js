45ae171dc61e4984cc7aac4ea71ef0ea34b3b24c
Michał Niżyński <mic.nizynski@gmail.com>
2021-11-28 13:28:12 +0100

    leave only build files

--- src/scripts/menu.js
+++
@@ -1,5 +0,0 @@
-export const toggleMenu = (menuButton) => {
-  const menuList = document.querySelector(".start-section__nav-list");
-  menuList.classList.toggle("active");
-  menuButton.classList.toggle("active");
-};
--- src/scripts/scroll.js
+++
@@ -1,5 +0,0 @@
-export const scrollTo = (value) => {
-  document
-    .getElementById(`${value}-section`)
-    .scrollIntoView({ behavior: "smooth" });
-};
